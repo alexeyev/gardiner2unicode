9f20429bc88c7e3aa234c652955c4fbd281060f6
alexeyev <anton.m.alexeyev@gmail.com>
2021-12-11 07:37:48 +0300

    [ENH] Fonts-related bug fixed

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 setup(
     name="gardiner2unicode",
     packages=setuptools.find_packages(),
-    version="0.0.1",
+    version="0.0.2",
     description="Mapping Gardiner's codes to Unicode + generating corresponding images.",
     long_description=long_description,
     long_description_content_type="text/markdown",
