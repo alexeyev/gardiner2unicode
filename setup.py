c32c610d6f0c06ead7d9d0f9765cea406b5a4296
Anton Alekseev <anton.m.alexeyev@gmail.com>
2025-07-20 00:36:17 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r", encoding="utf-8") as f:
 setup(
     name="gardiner2unicode",
     packages=find_packages(),
-    version="0.0.2",
+    version="0.0.3",
     description="Mapping Gardiner's codes to Unicode + generating corresponding images.",
     long_description=long_description,
     long_description_content_type="text/markdown",
